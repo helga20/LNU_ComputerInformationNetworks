cb6bc62cb7f6338c22bc01c99c4ec5aa39e34bf7
Olia Kravets <94845931+helga20@users.noreply.github.com>
2024-02-27 18:33:10 +0200

    Add files via upload

---
+++ Practice/Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            IPAddress address = Dns.GetHostEntry("localhost").AddressList[0];
+            Socket socket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(address, 8086);
+
+            socket.Connect(endPoint);
+
+            Console.WriteLine("Connected");
+            Console.WriteLine("Enter your name");
+
+            string name = Console.ReadLine();
+            byte[] d = Encoding.ASCII.GetBytes(name);
+            socket.Send(d);
+            socket.Close();
+        }
+
+    }
+}
---
+++ Practice/Sockets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Sockets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress adr = Dns.GetHostEntry("localhost").AddressList[0];
+            Socket Listener = new Socket(adr.AddressFamily, SocketType.Stream, ProtocolType.Tcp); 
+
+            IPEndPoint iPEnd = new IPEndPoint(adr, 8086);
+
+            Listener.Bind(iPEnd); 
+            Listener.Listen(10); 
+            Console.WriteLine("Waiting for connection");
+            
+            Socket socket = Listener.Accept();  
+            byte[] buffer = new byte[1024]; 
+
+          
+            socket.Receive(buffer); 
+            string data = Encoding.ASCII.GetString(buffer); 
+            Console.WriteLine($"Hello {data} !!!"); 
+
+
+            socket.Close();  
+        }
+
+    }
+}
